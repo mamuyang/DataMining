5bce760e7e0a30a6348242e864f132d7a75202de
Catherine <805274957@qq.com>
2018-10-26 13:59:05 +0800

    Update VSM.py

--- VSM.py
+++ VSM.py
@@ -1,136 +1,176 @@
 # encoding=utf-8
 import os
-from textblob import TextBlob
 import nltk as nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
-import numpy as np
-import random
-import shutil
-
-def split_data():
-    '''
-    split training set and testing set
-    :return:
-    '''
-    corpus_path = '20news-18828/20news-18828/'
-    trainPath = 'data/train/'
-    testPath = 'data/test/'
-    dict = {}
-    catelist = os.listdir(corpus_path)  # get all subcategorys under this categroy
-    sum = 0
-    for mydir in catelist:
-        class_path = corpus_path + mydir + "/"  # get the path based subcategroy
-        file_list = os.listdir(class_path)  # make a list about all sublist
-        num = len(file_list)
-        train_num = int(num * 0.8)
-        print(mydir)
-        print(train_num)
-        sum += train_num
-        train_list = random.sample(file_list, train_num)
-        for doc in file_list:
-            fullname = class_path + doc
-            dict['name'] = doc
-            dict['class'] = mydir
-            src = os.path.join(class_path, doc)
-            if doc in train_list:
-                des = trainPath + mydir + '_' + doc
-                shutil.copy(src, des)
-            else:
-                des = testPath + mydir + '_' + doc
-                shutil.copy(src, des)
-    print(sum)
-    return dict
-
-def readfile(path):
-    fp = open(path, "r")
-    content = fp.read()
-    fp.close()
-    return content
+import pickle
+import collections
+import math
 
 corpus_path = '20news-18828/20news-18828/'
 temp_path = 'temp.txt'
 
-def mergeData():
-    '''
-    merge all data into one txt
-    '''
-    catelist = os.listdir(corpus_path)  #get all subcategorys under this categroy
-    for mydir in catelist:
-        class_path = corpus_path + mydir + "/"  #get the path based subcategroy
-        file_list = os.listdir(class_path) #make a list about all sublist
-        for file_path in file_list:
-            fullname = class_path + file_path #path + XX.txt
-            print("The text you read is:",fullname)  #corpus/train/pos/pos1.txt
-            content = readfile(fullname)
-            with open(temp_path, 'a+') as w:
-                w.write(content)
-    print('processing over......')
 
-def process():
+def readfile(path):
     '''
-    data process including tokenization,stopwords filtering,stemming ,and get the dictionary
+    reading content from path
+    :param path:
     :return:
     '''
-    with open(temp_path,'r') as f:
-        content = readfile(temp_path).decode('utf-8','ignore')
-        # print(content)
-    stop_words = set(stopwords.words('english'))
-    word_tokens = word_tokenize(content)
-    # filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    with open(path, 'r') as f:
+        content = f.read().decode('utf-8','ignore')
+    return content
 
-    filtered_sentence = set()
-    for w in word_tokens:
-        if w not in stop_words:
-            filtered_sentence.add(w)
-
-    # vocab = set()
-    # stemmer = PorterStemmer()  # Stemming
-    # for word in filtered_sentence:
-    #     new_word = stemmer.stem(word)
-    #     vocab.add(new_word)
-
-    print(len(filtered_sentence)) #279767
-    return filtered_sentence
-
-
-def doc2onehot_tf_matrix(vocab):
+def readfiles():
     '''
-      transform document to onehot vector
+    reading file based root-dict
+    :return:
     '''
-    M = 18828  # 18828
-    V = len(vocab)  # 279767
-    onehot = np.zeros((M, V))
-    docs =[]
+    result = {}
     catelist = os.listdir(corpus_path)
     for mydir in catelist:
         class_path = corpus_path + mydir + "/"
         file_list = os.listdir(class_path)
-        for file_path in file_list:
-            fullname = class_path + file_path  # path + XX.txt
-            print("The text you read is:", fullname)  # corpus/train/pos/pos1.txt
-            doc = readfile(fullname).decode('utf-8','ignore')
-            docs.append(doc)
-    for d, doc in enumerate(docs):
-        for word in doc:
-            if word in vocab:
-                pos = vocab.index(word)
-                onehot[d][pos] = 1
-    return onehot
+        for file in file_list:
+            path = os.path.join(class_path, file)
+            text = readfile(path)
+            result[path] = text
+    return result
 
+def writefile(content,path):
+    '''
+    write content to path
+    :param content:
+    :param path:
+    :return:
+    '''
+    with open(path, 'wb') as wr:
+        pickle.dump(content, wr)  # 把dic保存到pickle文件里
 
-def random_copyfile(srcPath, dstPath, numfiles):
-    name_list = list(os.path.join(srcPath, name) for name in os.listdir(srcPath))
-    random_name_list = list(random.sample(name_list, numfiles))
-    if not os.path.exists(dstPath):
-        os.mkdir(dstPath)
-    for oldname in random_name_list:
-        shutil.copyfile(oldname, oldname.replace(srcPath, dstPath))
+# def mergeData():
+#     '''
+#     merge all data into one txt
+#     '''
+#     catelist = os.listdir(corpus_path)  #get all subcategorys under this categroy
+#     for mydir in catelist:
+#         class_path = corpus_path + mydir + "/"  #get the path based subcategroy
+#         file_list = os.listdir(class_path) #make a list about all sublist
+#         for file_path in file_list:
+#             fullname = class_path + file_path #path + XX.txt
+#             print("The text you read is:",fullname)  #corpus/train/pos/pos1.txt
+#             content = readfile(fullname)
+#             with open(temp_path, 'a+') as w:
+#                 w.write(content)
+#     print('processing over......')
 
+def process():
+    '''
+    data-process including tokenization,stopwords filtering,stemming(version unmatched???)
+    get the dictionary and vector representation
+    :return:
+    '''
 
+    text_dict = readfiles()
+    writefile(text_dict, 'text_dict.pkl')
+    word_tokens =[]
+    tf_dict = {}
+    for doc in text_dict:
+        content = text_dict[doc]
+        tokens = word_tokenize(content)
+        word_tokens.extend(tokens) # not append!!!!list convert to set cannot hash!!!
+        tf_dict[doc] = collections.Counter(tokens)
 
-words = process()
-vocab = sorted(set(words), key=words.index)
-doc2onehot_tf_matrix(vocab)
+    writefile(tf_dict,'tf_dict.pkl')
+    print(len(word_tokens))
 
+    word_dict = set(word_tokens)
+    print(word_dict) #
+    word_dict1 =set()
+    stop_words = set(stopwords.words('english'))
+    for w in word_dict:
+        if w not in stop_words:
+            word_dict1.add(w)
+    word_dict = word_dict1
+    print('word_dict:',len(word_dict)) #279765
+
+    word_dict = list(word_dict)
+    word_dict.sort()
+
+    len_word_dict = len(word_dict) #279765
+    num_of_doc = len(tf_dict) #18828
+    print('num of doc:',num_of_doc)
+    print('-------------df caculate-----------')
+    df = [0] * len_word_dict
+    for key in tf_dict:
+        for i in range(len_word_dict):
+            if word_dict[i] in tf_dict[key]:
+                df[i] = df[i] + 1
+    idf = [math.log(num_of_doc / t) for t in df]
+
+    thresh = [1 if int(t) >= 50 else 0 for t in df]
+    # setting threshhold to filter df with lower num
+
+    word_dict_new = []
+    df_new = []
+    idf_new = []
+    for i in range(len(df)):
+        if thresh[i] == 1:
+            word_dict_new.append(word_dict[i])
+            df_new.append(df[i])
+            idf_new.append(idf[i])
+    # writefile(word_dict_new, 'word_dict_new.pkl')
+    # writefile(df_new, 'df_new.pkl')
+    # writefile(idf_new, 'idf_new.pkl')
+
+    len_dict_new = len(word_dict_new) #6343
+    print('The size of new dict is:',len_dict_new)
+    vector_space = []
+    print('start building vector space model...')
+    for key in tf_dict:
+        vector = [key]
+        vector.extend([0] * len_dict_new)
+        for i in range(len_dict_new):
+            if word_dict_new[i] in tf_dict[key]:
+                vector[i + 1] = (1 + math.log(tf_dict[key][word_dict_new[i]])) * idf_new[i]  # tf * idf
+        vector_space.append(vector)
+    writefile(vector_space, 'vector_space.pkl')
+    print('vector space model build over......')
+    return
+
+# def doc2onehot_tf_matrix(vocab):
+#     '''
+#       transform document to onehot vector
+#     '''
+#     M = 18828  # 18828
+#     V = len(vocab)  # 279767
+#     onehot = np.zeros((M, V))
+#     docs =[]
+#     dict = {}
+#     catelist = os.listdir(corpus_path)
+#     for mydir in catelist:
+#         class_path = corpus_path + mydir + "/"
+#         file_list = os.listdir(class_path)
+#         for file_path in file_list:
+#             fullname = class_path + file_path  # path + XX.txt
+#             print("The text you read is:", fullname)  # corpus/train/pos/pos1.txt
+#             dict['name'] = fullname
+#             dict['class'] = mydir
+#             doc = readfile(fullname).decode('utf-8','ignore')
+#             dict['content'] = doc
+#             docs.append(doc)
+#     for d, doc in enumerate(docs):
+#         for word in doc:
+#             if word in vocab:
+#                 pos = vocab.index(word)
+#                 onehot[d][pos] = 1
+#     print('matrix build over.....')
+#     with open('data/dict.pickle','wb') as wr:
+#         pickle.dump(dict,wr)
+#     with open('data/doc2vec.pickle', 'wb') as wr:
+#         pickle.dump(onehot, wr)  # 把dic保存到pickle文件里
+#
+#     return onehot
+
+if __name__ == '__main__':
+    process()
